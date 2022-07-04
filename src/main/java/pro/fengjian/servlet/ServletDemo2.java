f19e0eeb6188b49825ab011e66067df07246bfe8
fengjian2705 <fengjian2705@sina.com>
2022-07-04 11:18:29 +0800

    feat: 获取请求参数;

--- src/main/java/pro/fengjian/servlet/ServletDemo2.java
+++ src/main/java/pro/fengjian/servlet/ServletDemo2.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Map;
 
 public class ServletDemo2 extends HttpServlet {
 
@@ -13,18 +14,31 @@ public class ServletDemo2 extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         System.out.println("---------------------请求行");
-        System.out.println("请求方式: "+ req.getMethod());
-        System.out.println("虚拟路径: "+ req.getContextPath());
-        System.out.println("请求 url: "+req.getRequestURL());
-        System.out.println("请求协议和版本号: "+ req.getProtocol());
-        System.out.println("请求 IP: "+req.getRemoteAddr());
+        System.out.println("请求方式: " + req.getMethod());
+        System.out.println("虚拟路径: " + req.getContextPath());
+        System.out.println("请求 url: " + req.getRequestURL());
+        System.out.println("请求协议和版本号: " + req.getProtocol());
+        System.out.println("请求 IP: " + req.getRemoteAddr());
 
         System.out.println("-------------------请求头");
-        System.out.println("Host: "+ req.getHeader("host"));
+        System.out.println("Host: " + req.getHeader("host"));
         System.out.println("所有请求头: ");
         Enumeration headerNames = req.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             System.out.println(headerNames.nextElement());
+        }
+
+        System.out.println("-----------------请求体");
+        System.out.println("getParameter: " + req.getParameter("name"));
+        System.out.println("getParameters: " );
+        for (String name : req.getParameterValues("name")) {
+            System.out.print(name);
+        }
+        System.out.println();
+        System.out.println("getParameterMap: ");
+        Map<String,String>parameterMap = req.getParameterMap();
+        for (Map.Entry<String, String> stringStringEntry : parameterMap.entrySet()) {
+            System.out.println(stringStringEntry.getKey()+" : "+stringStringEntry.getValue());
         }
     }
 
