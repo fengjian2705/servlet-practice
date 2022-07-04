3fd6877976beaf7fa92049fce5e568cbedef4c05
fengjian2705 <fengjian2705@sina.com>
2022-07-04 11:07:31 +0800

    feat: 请求头;

--- src/main/java/pro/fengjian/servlet/ServletDemo2.java
+++ src/main/java/pro/fengjian/servlet/ServletDemo2.java
@@ -5,12 +5,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class ServletDemo2 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doGet...");
+
+        System.out.println("---------------------请求行");
+        System.out.println("请求方式: "+ req.getMethod());
+        System.out.println("虚拟路径: "+ req.getContextPath());
+        System.out.println("请求 url: "+req.getRequestURL());
+        System.out.println("请求协议和版本号: "+ req.getProtocol());
+        System.out.println("请求 IP: "+req.getRemoteAddr());
+
+        System.out.println("-------------------请求头");
+        System.out.println("Host: "+ req.getHeader("host"));
+        System.out.println("所有请求头: ");
+        Enumeration headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            System.out.println(headerNames.nextElement());
+        }
     }
 
     @Override
